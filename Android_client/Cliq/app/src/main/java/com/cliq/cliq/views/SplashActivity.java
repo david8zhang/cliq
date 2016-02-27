4bd1643b2ec44d9c68aac6d5e909b0ce049176a6
David Zhang <david8zhang@berkeley.edu>
2016-02-27 14:30:18 -0800

    added splash screen code

--- Android_client/Cliq/app/src/main/java/com/cliq/cliq/views/SplashActivity.java
+++ Android_client/Cliq/app/src/main/java/com/cliq/cliq/views/SplashActivity.java
@@ -1,5 +1,9 @@
 package com.cliq.cliq.views;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +11,48 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class SplashActivity extends AppCompatActivity{
 
+    SharedPreferences mPrefs;
+
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String user_id = mPrefs.getString("user_id", null);
+        Thread homeThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);
+                } catch (Exception e) {
+                    System.out.println(e);
+                } finally {
+                    Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        Thread onboardThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);
+                } catch (Exception e) {
+                    System.out.println(e);
+                } finally {
+                    Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        if(user_id == null) {
+            onboardThread.start();
+        } else {
+            homeThread.start();
+        }
+    }
 
 }
