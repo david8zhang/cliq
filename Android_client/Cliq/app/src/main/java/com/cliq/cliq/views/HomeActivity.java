fa2dba9c10c07ae97bbef039b3050a1087375413
David Zhang <david8zhang@berkeley.edu>
2016-02-27 14:08:49 -0800

    added some good stuff

--- Android_client/Cliq/app/src/main/java/com/cliq/cliq/views/HomeActivity.java
+++ Android_client/Cliq/app/src/main/java/com/cliq/cliq/views/HomeActivity.java
@@ -1,7 +1,23 @@
 package com.cliq.cliq.views;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.cliq.cliq.R;
+import com.cliq.cliq.api.ApiManager;
+
 /**
  * Created by david_000 on 2/27/2016.
  */
-public class HomeActivity {
+public class HomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
 }
